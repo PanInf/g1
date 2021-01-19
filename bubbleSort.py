c72b4d907e2ce649d775a67bbfcc4603b6085255
PanInf <paninf@paninf.pl>
2021-01-19 12:33:45 +0000

    Change .repl.it and bubbleSort.py (adding example section)

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,3 +1,4 @@
+# Function
 def bubbleSort(T):
     n = len(T)
     for j in range(n-1, 0, -1):
@@ -5,4 +6,10 @@ def bubbleSort(T):
             if T[i] > T[i+1]:
                 T[i], T[i+1] = T[i+1], T[i]
 
+#Example
+from random import randint
 
+Y = [randint(1,100) for i in range(20)]
+print(Y)
+bubbleSort(Y)
+print(Y)
